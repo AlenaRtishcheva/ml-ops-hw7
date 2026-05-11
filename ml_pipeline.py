cddb2969cb0971953435a593a4e57254cc6fdcda
Алена <al.rtisheva@mail.ru>
2026-05-11 23:47:06 +0300

    Step 3: Implement Canary Deployment with Nginx

--- ml_pipeline.py
+++ ml_pipeline.py
@@ -3,6 +3,8 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import time
+time.sleep(3600)
 
 # 1. Загружаем данные
 iris = load_iris()
@@ -18,3 +20,5 @@ model.fit(X_train, y_train)
 # 4. Проверяем точность
 accuracy = accuracy_score(y_test, model.predict(X_test))
 print(f'Точность accuracy: {accuracy:.2f}')
+
+time.sleep(3600)
